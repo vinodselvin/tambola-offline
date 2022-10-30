0d7005c9c15fd6932c105404c24078db2eab3648
VINOD SELVIN NADAR <vinod.selvin@deliverysolutions.co>
2022-10-30 21:59:16 +0530

    Responsive fix

--- src/components/ticket.js
+++ src/components/ticket.js
@@ -94,88 +94,39 @@ class Ticket extends React.Component {
                         <span className="w-name">Welcome, <span className="a-name">{this.state.username}</span></span>
                     </Grid>
                 </Grid>
-                <Grid container >
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[0] == this.state.selectedSeries[0] ? 'active' : '')} onClick={() => this.ticketClicked(0)}>{this.state.series[0]}</div>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <div class="ticket"></div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[1] == this.state.selectedSeries[1] ? 'active' : '')} onClick={() => this.ticketClicked(1)}>{this.state.series[1]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[2] == this.state.selectedSeries[2] ? 'active' : '')} onClick={() => this.ticketClicked(2)}>{this.state.series[2]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class="ticket"></div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[3] == this.state.selectedSeries[3] ? 'active' : '')} onClick={() => this.ticketClicked(3)}>{this.state.series[3]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[4] == this.state.selectedSeries[4] ? 'active' : '')} onClick={() => this.ticketClicked(4)}>{this.state.series[4]}</div>
-                    </Grid>
-                    <Grid item xs={2}></Grid>
-                </Grid>
-                <Grid container >
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={1}>
-                        <div class="ticket"></div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[5] == this.state.selectedSeries[5] ? 'active' : '')} onClick={() => this.ticketClicked(5)}>{this.state.series[5]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[6] == this.state.selectedSeries[6] ? 'active' : '')} onClick={() => this.ticketClicked(6)}>{this.state.series[6]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class="ticket"></div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[7] == this.state.selectedSeries[7] ? 'active' : '')} onClick={() => this.ticketClicked(7)}>{this.state.series[7]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[8] == this.state.selectedSeries[8] ? 'active' : '')} onClick={() => this.ticketClicked(8)}>{this.state.series[8]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class="ticket"></div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[9] == this.state.selectedSeries[9] ? 'active' : '')} onClick={() => this.ticketClicked(9)}>{this.state.series[9]}</div>
-                    </Grid>
-                    <Grid item xs={2}></Grid>
-                </Grid>
-                <Grid container >
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={1}>
-                        <div class="ticket"></div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[10] == this.state.selectedSeries[10] ? 'active' : '')} onClick={() => this.ticketClicked(10)}>{this.state.series[10]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[11] == this.state.selectedSeries[11] ? 'active' : '')} onClick={() => this.ticketClicked(11)}>{this.state.series[11]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[12] == this.state.selectedSeries[12] ? 'active' : '')} onClick={() => this.ticketClicked(12)}>{this.state.series[12]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class="ticket"></div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[13] == this.state.selectedSeries[13] ? 'active' : '')} onClick={() => this.ticketClicked(13)}>{this.state.series[13]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class={"ticket " + (this.state.series[14] == this.state.selectedSeries[14] ? 'active' : '')} onClick={() => this.ticketClicked(14)}>{this.state.series[14]}</div>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <div class="ticket"></div>
-                    </Grid>
-                    <Grid item xs={2}></Grid>
-                </Grid>
-
+                <div class="gboard">
+                    <h2 align="center">#{this.state.username.toUpperCase()}</h2>
+                    <div class="d-flex">
+                        <span class={"ticket " + (this.state.series[0] == this.state.selectedSeries[0] ? 'active' : '')} onClick={() => this.ticketClicked(0)}>{this.state.series[0]}</span>
+                        <span class="ticket"></span>
+                        <span class={"ticket " + (this.state.series[1] == this.state.selectedSeries[1] ? 'active' : '')} onClick={() => this.ticketClicked(1)}>{this.state.series[1]}</span>
+                        <span class={"ticket " + (this.state.series[2] == this.state.selectedSeries[2] ? 'active' : '')} onClick={() => this.ticketClicked(2)}>{this.state.series[2]}</span>
+                        <span class="ticket"></span>
+                        <span class={"ticket " + (this.state.series[3] == this.state.selectedSeries[3] ? 'active' : '')} onClick={() => this.ticketClicked(3)}>{this.state.series[3]}</span>
+                        <span class={"ticket " + (this.state.series[4] == this.state.selectedSeries[4] ? 'active' : '')} onClick={() => this.ticketClicked(4)}>{this.state.series[4]}</span>
+                        <span class="ticket"></span>
+                    </div>
+                    <div class="d-flex">
+                        <span class="ticket"></span>
+                        <span class={"ticket " + (this.state.series[5] == this.state.selectedSeries[5] ? 'active' : '')} onClick={() => this.ticketClicked(5)}>{this.state.series[5]}</span>
+                        <span class={"ticket " + (this.state.series[6] == this.state.selectedSeries[6] ? 'active' : '')} onClick={() => this.ticketClicked(6)}>{this.state.series[6]}</span>
+                        <span class="ticket"></span>
+                        <span class={"ticket " + (this.state.series[7] == this.state.selectedSeries[7] ? 'active' : '')} onClick={() => this.ticketClicked(7)}>{this.state.series[7]}</span>
+                        <span class={"ticket " + (this.state.series[8] == this.state.selectedSeries[8] ? 'active' : '')} onClick={() => this.ticketClicked(8)}>{this.state.series[8]}</span>
+                        <span class="ticket"></span>
+                        <span class={"ticket " + (this.state.series[9] == this.state.selectedSeries[9] ? 'active' : '')} onClick={() => this.ticketClicked(9)}>{this.state.series[9]}</span>
+                    </div>
+                    <div class="d-flex">
+                        <span class={"ticket " + (this.state.series[10] == this.state.selectedSeries[10] ? 'active' : '')} onClick={() => this.ticketClicked(10)}>{this.state.series[10]}</span>
+                        <span class="ticket"></span>
+                        <span class={"ticket " + (this.state.series[11] == this.state.selectedSeries[11] ? 'active' : '')} onClick={() => this.ticketClicked(11)}>{this.state.series[11]}</span>
+                        <span class={"ticket " + (this.state.series[12] == this.state.selectedSeries[12] ? 'active' : '')} onClick={() => this.ticketClicked(12)}>{this.state.series[12]}</span>
+                        <span class="ticket"></span>
+                        <span class={"ticket " + (this.state.series[13] == this.state.selectedSeries[13] ? 'active' : '')} onClick={() => this.ticketClicked(13)}>{this.state.series[13]}</span>
+                        <span class={"ticket " + (this.state.series[14] == this.state.selectedSeries[14] ? 'active' : '')} onClick={() => this.ticketClicked(14)}>{this.state.series[14]}</span>
+                        <span class="ticket"></span>
+                    </div>
+                </div>
                 <Grid container>
                     <Grid item xs={12} style={{textAlign: "center", fontSize: "20px"}}>
                         <p>Full Housie: {this.state.selectedSeries?.includes(0) ? "No" : "Yes"}</p>
